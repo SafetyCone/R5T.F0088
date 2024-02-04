329b6734025d98fa7658fb2261f719346799eafa
davidcoats <david@getsafetycone.com>
2024-02-03 18:23:18 -0800

    Interim changes.

--- source/R5T.F0088/Code/Functionality/IVisualStudioOperator.cs
+++ source/R5T.F0088/Code/Functionality/IVisualStudioOperator.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 using CliWrap;
 
-using R5T.F0078;
+using R5T.F0078.Extensions;
 using R5T.T0132;
 
 
@@ -12,7 +12,7 @@ namespace R5T.F0088
 	[FunctionalityMarker]
 	public partial interface IVisualStudioOperator : IFunctionalityMarker
 	{
-		public void OpenSolutionFile(string solutionFilePath)
+		public void Open_SolutionFile(string solutionFilePath)
 		{
 			var command = Cli.Wrap(@"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe")
 				.WithArguments(argumentsBuilder =>
